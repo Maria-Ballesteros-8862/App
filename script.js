1363e7ca8e3f00efb3223d86e60af11f8e422ed5
Maria <mariabg27200@gmail.com>
2023-09-17 01:57:03 -0500

    Completadas clases03 y clase04

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const btn = document.querySelector("[data-form-btn]");
-console.log(btn);
 
 // listener: el método que nos va a ayudar a generar un listener es AaddEventListener.
 //las arrow functions (funciones flecha) o también conocidas como funciones anónimas.
@@ -12,15 +11,35 @@ const createTask = (evento) => {
     const task = document.createElement('li');
     input.value = '';
     task.classList.add("card");
-    const content =  `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div> 
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('Task');
+    taskContent.appendChild(titleTask);
+    titleTask.innerText = value
+    const content =  `
+        ${checkComplete()}; 
         <i class="fas fa-trash-alt trashIcon icon"></i>`; 
     //backticks 
-    task.innerHTML = content;
+   // task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-    console.log(content);
-}
+};
+
 
 btn.addEventListener ("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i")
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask)
+    return i; 
+};
+
+// Imediately invaked function expression (IIFE)
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIncon');
+    element.classList.toggle('far');
+};
