299af7b5bf1761c5716d08c047b7872324c4a477
Maria <mariabg27200@gmail.com>
2023-09-17 04:46:57 -0500

    clase05

--- script.js
+++ script.js
@@ -1,48 +1,33 @@
-( () => {
+import checkComplete from "./Components/checkComplete"
+import deleteIcon from "./Components/deleteIcon"
+
 const btn = document.querySelector("[data-form-btn]");
 
-// listener: el método que nos va a ayudar a generar un listener es AaddEventListener.
-//las arrow functions (funciones flecha) o también conocidas como funciones anónimas.
+/* listener: el método que nos va a ayudar a generar un listener es AaddEventListener.*/
 
 const createTask = (evento) => {
     evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
+    const input = document.querySelector('[data-form-input]');
     const value = input.value;
-    const list = document.querySelector("[data-list]");
+    const list = document.querySelector('[data-list]');
     const task = document.createElement('li');
-    input.value = '';
     task.classList.add("card");
+    input.value = '';
     const taskContent = document.createElement('div');
-    taskContent.appendChild(checkComplete());
+
     const titleTask = document.createElement('span');
-    titleTask.classList.add('Task');
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    titleTask.innerText = value
-    const content =  `
-        ${checkComplete()}; 
-        <i class="fas fa-trash-alt trashIcon icon"></i>`; 
-    //backticks 
-   // task.innerHTML = content;
+
+    /*backticks*/
+    /*task.innerHTML = content;*/
     task.appendChild(taskContent);
+    taskContent.appendChild(deleteIcon());
     list.appendChild(task);
 };
 
 
 btn.addEventListener ("click", createTask);
-
-const checkComplete = () => {
-    const i = document.createElement("i")
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener('click', completeTask)
-    return i; 
-};
-
-// Imediately invaked function expression (IIFE)
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('completeIncon');
-    element.classList.toggle('far');
-};
-
-});
+//las arrow functions (funciones flecha) o también conocidas como funciones anónimas.
